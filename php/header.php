bfdea54ff382a0296c36c2625e028826da4642f6
Arshad272 <arshadkareem272@gmail.com>
2022-06-24 23:48:29 +0530

    V1.0.0

---
+++ php/header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<div class="header">
+        <div class="header-image">
+            <img style="display: block;float:left;" class="header-image1" src="images/header-image-counscode.png" alt="">
+        </div>
+        <div class="header-img">
+            <img class="header-image2" src="images/sietk-logo.png" alt="">
+            <img class="header-image3" src="images/header-image-clgname.png" alt="">
+        </div>
+        <div class="header-image-phone">
+        <img src="images/header-image.png" alt="">
+        </div>
+    </div>
+
+    <div class="header-navbar">
+
+        <div class="topnav" id="myTopnav">
+
+            <a href="#home" class="active">Home</a>
+            <a href="#news">About Us</a>
+            <div class="dropdown">
+                <button class="dropbtn">Academics
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Civil</a>
+                    <a href="#">Mechanical</a>
+                    <a href="#">EEE</a>
+                    <a href="#">ECE</a>
+                    <div class="dropdown-right">
+                        <a class="dropbtn-right">Computer Science&nbsp;&nbsp;<i class="fa fa-caret-right"></i></a>
+                        <div id="dropdown-content-right" class="dropdown-content-right">
+                            <a href="#">CSE</a>
+                            <a href="#">CSIT</a>
+                            <a href="#">CSE - AI</a>
+                            <a href="#">CSE - ML</a>
+                            <a href="#">CSE - IOT</a>
+                        </div>
+                    </div>
+                    <a href="#">Agricultural</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Admissions
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Engineering</a>
+                    <a href="#">MBA</a>
+                    <a href="#">MCA</a>
+                    <a href="#">Diploma</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Placements
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">About Placement</a>
+                    <a href="#">Faculty</a>
+                    <a href="#">Facilities</a>
+                    <a href="#">Placement Records</a>
+                    <a href="#">Knowledge Center</a>
+                    <a href="#">Registration</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Activities
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">ED CELL</a>
+                    <a href="#">Grievance</a>
+                    <a href="#">Gallery</a>
+                    <a href="#">NSS</a>
+                    <a href="#">SIETK WEC</a>
+                    <a href="#">Alumni</a>
+                    <a href="#">Anti Ragging</a>
+                    <a href="#">Sports</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Exam Cell
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">About Exam Cell</a>
+                    <a href="#">Notifications</a>
+                    <a href="#">Academic Calenders</a>
+                    <a href="#">Time Tables</a>
+                    <div class="dropdown-right">
+                        <a class="dropbtn-right">Results&nbsp;&nbsp;<i class="fa fa-caret-right"></i></a>
+                        <div id="dropdown-content-right" class="dropdown-content-right">
+                            <a href="#">JNTUA</a>
+                            <a href="#">Autonomous</a>
+                        </div>
+                    </div>
+                    <div class="dropdown-right">
+                        <a class="dropbtn-right">Syllabus&nbsp;&nbsp;<i class="fa fa-caret-right"></i></a>
+                        <div id="dropdown-content-right" class="dropdown-content-right">
+                            <a href="#">UG</a>
+                            <a href="#">PG</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#contact">Vision & Mission</a>
+            <a href="#contact">Contact Us</a>
+            <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>
+        <div class="callout">
+            <div class="callout-header">FOR ADMISSIONS</div>
+            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+            <div class="callout-container">
+                <img src="images/admissions-contact.png" alt="">
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
